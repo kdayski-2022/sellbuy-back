5ab70343f60c64bfa63f7a318d92704dd49f42bd
TechDanil <danilkomarov243242@gmail.com>
2022-06-16 10:14:22 +0500

    commit 16.06.22
    commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jsonData = require('./data/data.js')
 const app = new express()
 const port = 3001
 const currentDayOfWeek = Date.parse("2022-06-13 00:00:00")
-const lastDayofWeek = Date.parse("2022-06-20 00:00:00")
+const lastDayofWeek = Date.parse("2022-06-24 00:00:00")
 const apiUrl = 'https://deribit.com/api/v2/public'
 const strikeStep = 100
 app.use(cors())
@@ -22,29 +22,56 @@ app.listen(port, async function () {
 })
 
 app.get('/test', async (req, res) => {
-    res.json(test())
+   const result = axios.get(`${apiUrl}/get_book_summary_by_currency?currency=ETH&kind=option`).then(res =>{
+            const fillteredDate = res.data.result.filter(item =>{
+            const [_, stortedDataUnderlying_index] = item.underlying_index.split('-')
+            var date = new Date(Date.parse(stortedDataUnderlying_index)).getTime() 
+    
+            return (date >= currentDayOfWeek && date <= lastDayofWeek)
+        })
+    
+        console.log("fillteredDate", fillteredDate)
+    
+    }).catch(err => console.log(err.response.data))
+
+    res.json({result})
 })
+
+
 app.get('/get_prices', async (req, res) => {
-    const currentPrice = 1213.12 // TODO Получить цену текущую
-    const formatedCurrentPrice = Math.round(currentPrice / 100) * 100
-    const prices = []
-    for (i = -4; i <= 4; i++) prices.push(formatedCurrentPrice + i * strikeStep)
-    res.json({ success: true, data: { currentPrice, prices } })
+    
+    axios.get(`${apiUrl}/get_book_summary_by_currency?currency=ETH&kind=option`).then(res =>{
+
+        
+    })
+
+    var currentPrice // TODO Получить цену текущую
+        var prices = []
+        jsonData.forEach(element => {
+             currentPrice = element.underlying_price
+        });
+    
+        console.log("currentPrice", currentPrice)
+        const formatedCurrentPrice = Math.round(currentPrice / 100) * 100
+        for (i = -4; i <= 4; i++) prices.push(formatedCurrentPrice + i * strikeStep)
+        res.json({ success: true, data: { currentPrice, prices } })
 })
-app.get('/get_periods', async (req, res) => {
-    const periods = [
-        { title: 'Tomorow', key: 'tomorow' },
-        { title: '1 Week', key: 'week' },
-        { title: 'One Week', key: '1week' },
-        { title: 'Two Week', key: '2week' },
-        { title: 'One Month', key: '1month' },
-    ]
-    res.json({ success: true, data: { periods } })
-})
-app.post('/order', (req, res) => {
-    console.log(req.body)
-    res.json({ success: true })
-})
+
+// app.get('/get_periods', async (req, res) => {
+//     const periods = [
+//         { title: 'Tomorow', key: 'tomorow' },
+//         { title: '1 Week', key: 'week' },
+//         { title: 'One Week', key: '1week' },
+//         { title: 'Two Week', key: '2week' },
+//         { title: 'One Month', key: '1month' },
+//     ]
+//     res.json({ success: true, data: { periods } })
+// })
+
+// app.post('/order', (req, res) => {
+//     console.log(req.body)
+//     res.json({ success: true })
+// })
 function getStrikes() {
 
     let strikes = []
@@ -58,35 +85,10 @@ function getStrikes() {
     //TODO Вытащить все старйки интересующих нас цен
     // console.log(strikes)
 }
-getStrikes()
-// test()
-// getData()
-function getData() {
-    axios.get(`${apiUrl}/get_book_summary_by_currency?currency=ETH&kind=option`).then(res => {
-        console.log(res.data.result.length)
-        for (i of res.data.result) {
-            console.log(i)
-            break
-        }
-    }).catch(err => console.log(err.response.data))
-}
-function test() {
-    const fillteredDate = jsonData.filter(item => {
-        const [_, stortedDataUnderlying_index] = item.underlying_index.split('-')
-        // const [first_part,stortedDataInstrument_name] = item.instrument_name.split('-')
-        var date = new Date(Date.parse(stortedDataUnderlying_index)).getTime()
-        // var dateInstrument_name = new Date(Date.parse(stortedDataInstrument_name)).getTime()
-        // console.log("instrument name", dateInstrument_name, "instrument name string", stortedDataInstrument_name)
-        // console.log("currentDayOfWeek: ", currentDayOfWeek)
-        // console.log("currentDayOfWeek: ", new Date(currentDayOfWeek).toString())
-        // console.log("lastDayofWeek: ", lastDayofWeek)
-        // console.log("lastDayofWeek: ", new Date(lastDayofWeek).toString())
-        // console.log("date: ", date)
-        // console.log("date: ", new Date(date).toString())
-        return (date >= currentDayOfWeek && date <= lastDayofWeek)
-    })
+// getStrikes()
+// // test()
 
-    console.log("fillteredDate", fillteredDate)
-    return fillteredDate
+function getData(){
+
 }
 
