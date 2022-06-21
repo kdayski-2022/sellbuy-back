cb0e94c1e4088e69d2b798be4288ada907171381
SeregaBiceps <58617773+SeregaBiceps@users.noreply.github.com>
2022-06-21 16:28:32 +0300

    merge

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
-
 const jsonData = require('./data/data.js')
 const app = new express()
 const port = 3001
-const currentDayOfWeek = Date.parse("2022-06-13 00:00:00")
-const lastDayofWeek = Date.parse("2022-06-24 00:00:00")
+const currentDayOfWeek = Date.parse("2022-06-20 00:00:00")
+const lastDayofWeek = Date.parse("2022-06-30 00:00:00")
 const apiUrl = 'https://deribit.com/api/v2/public'
 const strikeStep = 100
+
+const jsonDate =[]
+
 app.use(cors())
 app.use(express.json({ limit: '10mb' }))
 
@@ -43,25 +45,13 @@ app.get('/get_strikes', async(req, res) =>{
 
     })
 
-    const fillteredStrikes = jsonData.filter(item =>{
-        const strikes = []
-        const dateStrike = item.instrument_name.split('-')[1]
-        const strike = Number(item.instrument_name.split('-')[2])
-        const date = new Date(Date.parse(dateStrike)).getTime()
-        console.log("date" , date)
-        console.log("strike", strike)
-        strikes.push({ strike, date })
-        return (date =>currentDayOfWeek && date <=lastDayofWeek)
-    })
+    for (data of jsonData) {
+        const strike = Number(data.instrument_name.split('-')[2])
+        strikes.push({ strike, data })
+    }
+    for (data of strikes) {
 
-    res.json(fillteredStrikes)
-    // for (data of jsonData) {
-    //     const strike = Number(data.instrument_name.split('-')[2])
-    //     strikes.push({ strike, data })
-    // }
-    // for (data of strikes) {
-
-    // }
+    }
     //TODO Вытащить все старйки интересующих нас цен
 })
 
@@ -69,17 +59,13 @@ app.get('/get_strikes', async(req, res) =>{
 app.get('/get_prices', async (req, res) => {
     
     axios.get(`${apiUrl}/get_book_summary_by_currency?currency=ETH&kind=option`).then(apiRes =>{
-
-        let currentPrice // TODO Получить цену текущую
-        apiRes.data.result.forEach(price => {
-               currentPrice = price.underlying_price
-           });
-       
-    //    console.log("currentPrice", currentPrice)
-       const prices = []
-       const formatedCurrentPrice = Math.round(currentPrice / 100) * 100
-       for (i = -4; i <= 4; i++) prices.push(formatedCurrentPrice + i * strikeStep)
-       res.json({ success: true, data: { currentPrice, prices } })
+        
+        const currentPrice = apiRes.data.result[0].underlying_price// TODO Получить цену текущую
+        const prices = []
+        console.log("currentPrice", currentPrice)
+        const formatedCurrentPrice = Math.round(currentPrice / 100) * 100
+        for (i = -4; i <= 4; i++) prices.push(formatedCurrentPrice + i * strikeStep)
+        res.json({success:true, data:{currentPrice, prices}})
     })
 })      
 
@@ -89,12 +75,17 @@ app.get('/get_periods', async (req, res) => {
         { title: '1 Week', key: 'week' },
         { title: 'One Week', key: '1week' },
         { title: 'Two Week', key: '2week' },
-        { title: 'One Month', key: '1month' },
+        { title: 'One Month', key: '1month' }
     ]
     res.json({ success: true, data: { periods } })
 })
 
-app.post('/order', (req, res) => {
-    console.log(req.body)
-    res.json({ success: true })
+app.post('/get_order_data', (req, res) => {
+    const {period, price} = req.body
+
+    jsonData[period] = {
+        period, price
+    }
+    
+    res.json(jsonData[period])//пробежаться по массиву отфильтровать по дате и цене instrument name
 })
