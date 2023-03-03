3ffb7e68d3d7fcc6647e1f1d915feaf09328cf3e
demolit_fanil@mail.ru <demolit_fanil@mail.ru>
2023-03-03 14:14:59 +0300

    Add referrals

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,8 @@ const db = require('../database');
 const { writeLog, updateLog } = require('../lib/logger');
 const { checkSession } = require('../lib/session');
 const { parseError } = require('../lib/lib');
+const { COMMISSION } = require('../config/constants.json');
+const REF_FEE = process.env.REF_FEE;
 
 const generateRef = async () => {
   let ref_code = crypto.randomBytes(3).toString('hex');
@@ -63,6 +65,51 @@ const updateUser = async (user, data) => {
   }
 };
 
+const getReferralPayouts = async (referrals) => {
+  const referralsPayouts = [];
+  for (const referral of referrals) {
+    const payouts = await db.models.ReferralPayout.findAll({
+      where: {
+        address: referral.address,
+      },
+    });
+    referralsPayouts.push(...payouts);
+  }
+  return referralsPayouts;
+};
+
+const getOrders = async (referralsPayouts) => {
+  const orders = [];
+  for (const payout of referralsPayouts) {
+    const order = await db.models.Order.findOne({
+      where: {
+        id: payout.order_id,
+      },
+    });
+    order.referral_paid = payout.paid;
+    orders.push(order);
+  }
+  return orders;
+};
+
+const getRefTable = async (orders, ref_fee) => {
+  ref_fee = ref_fee || REF_FEE || 0;
+  const refTable = [];
+  for (const order of orders) {
+    if (order && order.order_complete && order.status === 'approved') {
+      const address = order.from;
+      const appRevenue = (order.recieve / (1 - COMMISSION)) * COMMISSION;
+      const earn = (appRevenue / 100) * Number(ref_fee);
+      refTable.push({
+        address,
+        earn: `$${earn.toFixed(2)}`,
+        paid: order.referral_paid,
+      });
+    }
+  }
+  return refTable;
+};
+
 class UserController {
   async getRef(req, res) {
     const sessionInfo = await checkSession(req);
@@ -85,13 +132,25 @@ class UserController {
         user = await createUser(address);
       }
       ref = user.ref_code;
-      const referrals = [
-        { address, earn: '$12' },
-        { address, earn: '$12' },
-      ];
+
+      const referrals = await db.models.User.findAll({
+        where: {
+          ref_user_id: user.id,
+        },
+      });
+      const referralsPayouts = await getReferralPayouts(referrals);
+      const orders = await getOrders(referralsPayouts);
+      const refTable = await getRefTable(orders, user.ref_fee);
 
       updateLog(logId, { status: 'success' });
-      res.json({ success: true, data: { ref, referrals }, sessionInfo });
+      res.json({
+        success: true,
+        data: {
+          ref,
+          referrals: refTable,
+        },
+        sessionInfo,
+      });
     } catch (e) {
       console.log(e);
       updateLog(logId, { status: 'failed', error: parseError(e) });
--- database/models/User.js
+++ database/models/User.js
@@ -4,4 +4,5 @@ module.exports = {
   address: DataTypes.STRING,
   ref_code: DataTypes.STRING,
   ref_user_id: DataTypes.INTEGER,
+  ref_fee: DataTypes.INTEGER,
 };
