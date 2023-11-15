b77229f762b427edc6a5561c22166f482070ecb4
Fanilus <fanil@demolit.ru>
2023-11-15 10:11:59 +0300

    executa price debug

--- service_script/emulateExpiration.js
+++ service_script/emulateExpiration.js
@@ -1,33 +1,32 @@
-// await db.models.Order.update(
-//   { status: 'pending_approve', chain_id: 80001 },
-//   {
-//     where: {
-//       [db.Op.and]: [
-//         { execute_date: { [db.Op.gt]: '2023-11-02' } },
-//         { execute_date: { [db.Op.lt]: '2023-11-04' } },
-//       ],
-//     },
-//   }
-// );
-// const orders = await db.models.Order.findAll({
-//   where: { status: 'pending_approve' },
-// });
-// try {
-//   const res = await axios.post(
-//     'http://dev.fanil.ru:5111/api/expiration',
-//     {
-//       orders,
-//       tx: '0x022b935c8ae6e92ca72903d73e2ef48e573d83cd037cfc25aec927105d3c274c',
-//     },
-//     {
-//       headers: {
-//         Accept: 'application/json',
-//         'Content-Type': 'application/json',
-//       },
-//     }
-//   );
-//   console.log(res);
-// } catch (e) {
-//   console.log(e);
-// }
-// resetOrders();
+const db = require('../database');
+
+db.connection.authenticate().then(async () => {
+  try {
+    // await db.models.Order.update(
+    //   {
+    //     execute_date: '2023-11-14',
+    //     order_complete: false,
+    //     status: 'created',
+    //     end_index_price: 1850,
+    //     settlement_date: null,
+    //     payout_base: null,
+    //     payout_usdc: null,
+    //     payout_tx: null,
+    //     order_executed: null,
+    //     payout_currency: null,
+    //     chain_id: 80001,
+    //   },
+    //   {
+    //     where: {
+    //       [db.Op.and]: [
+    //         { execute_date: { [db.Op.gt]: '2023-11-09' } },
+    //         { execute_date: { [db.Op.lt]: '2023-11-18' } },
+    //       ],
+    //     },
+    //   }
+    // );
+    console.log('done');
+  } catch (e) {
+    console.log(e);
+  }
+});
