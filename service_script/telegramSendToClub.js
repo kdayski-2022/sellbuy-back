29a4ed3fccd0339c7e7fcdb0fb46767131f6988a
demolit_fanil@mail.ru <demolit_fanil@mail.ru>
2023-12-22 12:18:00 +0300

    fix

---
+++ service_script/telegramSendToClub.js
@@ -0,0 +1,28 @@
+const { Telegraf } = require('telegraf');
+
+const CLUB_CHAT_ID = '';
+const TELEGRAM_KEY = '';
+
+function Telegram() {
+  if (!(this instanceof Telegram)) {
+    return new Telegram();
+  }
+  this.clubChatId = CLUB_CHAT_ID;
+  const bot = new Telegraf(TELEGRAM_KEY);
+  this.bot = bot;
+  bot.launch();
+}
+
+Telegram.prototype.send = async function send(msg) {
+  try {
+    await this.bot.telegram.sendMessage(this.clubChatId, msg, {
+      disable_notification: true,
+    });
+  } catch (e) {
+    console.log('On sending telegram message error');
+    console.log(e);
+  }
+};
+
+const telegram = new Telegram();
+telegram.send('TIME-LIMIT ORDER BUY 0.44 WBTC 43500 29DEC APR 69%');
